d98583e4224ce8e8cbf16263f2300f80d96bcaa1
Vitor Guedes <vgm_rox@hotmail.com>
2022-09-02 01:24:26 -0300

    Add profile user stats

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,6 +14,35 @@ const Profile = () => {
           <a href="https://google.com/">☰</a>
         </div>
       </div>
+
+      <div className="profile-user-picture-info">
+        <div className="profile-user-picture">
+          userImage
+        </div>
+        <div className="profile-posts-followers-following">
+          <div className="profile-posts-counter">
+            <p>74</p>
+            <p>Posts</p>
+          </div>
+          <div className="profile-followers-counter">
+            <p>419</p>
+            <p>followers</p>
+          </div>
+          <div className="profile-following-counter">
+            <p>692</p>
+            <p>Following</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="profile-username-biodescription">
+        <div>userName</div>
+        <div>bioDescription</div>
+      </div>
+
+      <div className="profile-edit-profile-section">
+        <div className="edit-profile">Edit profile</div>
+      </div>
     </div>
   );
 };
