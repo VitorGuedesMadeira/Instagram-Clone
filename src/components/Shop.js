3141f263e30df8e95ecdb813eff0fd330de7750c
Vitor Guedes <vgm_rox@hotmail.com>
2022-09-02 02:24:18 -0300

    add shop section

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Shop.css';
 
 const Shop = () => {
-  const shop = 'shop';
+  const userName = 'shop';
   return (
-    <div>
-      {shop}
+    <div className="shop-section">
+      <div className="shop-navigation-bar">
+        <div className="shop-title">
+          {userName}
+        </div>
+        <div className="shop-navigation-links">
+          <a href="https://google.com/">🗓️</a>
+          <a href="https://google.com/">☰</a>
+        </div>
+      </div>
     </div>
   );
 };
