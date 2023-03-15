6032e888bacc77c04df94fb43722af94f0b2386d
Nyame-Wolf <mumenyam@gmail.com>
2023-03-15 15:42:24 +0300

    Create a component to create a new post and its styles

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,12 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import postNewPost from '../../redux/thunks/newPostThunk';
+import uploadIcon from '../../assets/icons/upload.png';
 import './NewPost.scss';
 
 const NewPost = () => {
-  useEffect(() => {
-    window.scrollTo(20, 0);
-  });
+  const [images, setImages] = useState([]);
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
 
-  return <div id="new-post-section-wrapper">New post</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('post[user_id]', 1);
+    formData.append('post[title]', title);
+    images.forEach((image) => {
+      formData.append('post[images][]', image);
+    });
+    dispatch(postNewPost(formData));
+  };
+
+  const handleImageUpload = (event) => {
+    const files = Array.from(event.target.files);
+    setImages(files);
+  };
+
+  return (
+    <div className="modal">
+      <button type="submit" className="close">
+        &times;
+      </button>
+      <div className="modal-content">
+        <div className="modal-head">
+          <h2>Create new post</h2>
+        </div>
+        <div className="modal-body">
+          <img src={uploadIcon} alt="Drag images symbol" />
+          <h3>Drag photos and videos here</h3>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="file-upload" className="custom-file-upload">
+              Select From Computer
+              <input
+                id="file-upload"
+                type="file"
+                onChange={handleImageUpload}
+                accept="image/*"
+                multiple
+              />
+            </label>
+
+            <input
+              type="text"
+              placeholder="title"
+              name="title"
+              className="title-input-box"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+            <input className="post-submit" type="submit" value="Post" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewPost;
