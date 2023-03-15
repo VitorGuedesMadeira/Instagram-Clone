bfa74d1bdf58635109d105a74e4275f2776c9ad7
Nyame-Wolf <mumenyam@gmail.com>
2023-03-15 22:30:27 +0300

    add second popup for images

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -9,6 +9,8 @@ const NewPost = () => {
   const [images, setImages] = useState([]);
   const [title, setTitle] = useState('');
   const [fileLabel, setFileLabel] = useState('Select From Computer');
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -28,6 +30,8 @@ const NewPost = () => {
     const files = Array.from(event.target.files);
     setImages(files);
     setFileLabel(`Uploaded ${files.length} file${files.length > 1 ? 's' : ''}`);
+    setSelectedImage(URL.createObjectURL(files[0]));
+    setShowPopup(true);
   };
 
   const handleRedirection = () => navigate('/');
@@ -42,9 +46,47 @@ const NewPost = () => {
           <h2>Create new post</h2>
         </div>
         <div className="modal-body">
-          <img src={uploadIcon} alt="Drag images symbol" />
-          <h3>Drag photos and videos here</h3>
           <form onSubmit={handleSubmit}>
+            {showPopup && (
+              <div className="image-popup">
+                <div className="popup-head">
+                  <h2>Crop</h2>
+                  <button type="submit" className="next-btn">
+                    Next
+                  </button>
+                </div>
+                <img
+                  src={selectedImage}
+                  alt="Selected"
+                  className="upload-img"
+                />
+                <input
+                  type="text"
+                  placeholder="title"
+                  name="title"
+                  className="title-input-box"
+                  value={title}
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                  }}
+                />
+                {title !== '' && (
+                  <input
+                    className="post-submit"
+                    type="submit"
+                    value="Post"
+                    onClick={handleSubmit}
+                  />
+                )}
+              </div>
+            )}
+            {!showPopup && (
+              <div className="drag-and-drop">
+                <img src={uploadIcon} alt="Drag images symbol" />
+                <h3>Drag photos and videos here</h3>
+              </div>
+            )}
+
             <label htmlFor="file-upload" className="custom-file-upload">
               {fileLabel}
               <input
@@ -55,22 +97,6 @@ const NewPost = () => {
                 multiple
               />
             </label>
-
-            {images.length > 0 && (
-              <input
-                type="text"
-                placeholder="title"
-                name="title"
-                className="title-input-box"
-                value={title}
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-              />
-            )}
-            {title !== '' && (
-              <input className="post-submit" type="submit" value="Post" />
-            )}
           </form>
         </div>
       </div>
